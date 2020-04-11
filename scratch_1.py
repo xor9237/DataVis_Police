39a3d58a95d13651956293c150ba245d618334fa
xor9237 <ktlee927@gmail.com>
2020-04-11 11:27:40 -0700

    thrid

--- scratch_1.py
+++ scratch_1.py
@@ -16,18 +16,21 @@ df_new = df_new.reset_index()
 df_new = df_new.rename(columns={'PdDistrict':'Neighborhood', 'Resolution':'Count'})
 
 
+
 import folium
 
 
 sf_geo = r'/Users/kitaeklee/PycharmProjects/DataVis_Police/san-francisco.geojson'
 
 # Initialize the map
-world_map = folium.Map(location=[-122.4, 37.8], zoom_start=12, tiles='Mapbox Bright')
+world_map = folium.Map(location=[37, -122], zoom_start=12, tiles='OpenStreetMap')
 # Choropleth map
 world_map.choropleth(geo_data=sf_geo, data=df_new, columns=['Neighborhood', 'Count'],
                      key_on='feature.properties.DISTRICT', fill_color='YlOrRd',
                      fill_opacity=0.7, line_opacity=0.2,
                      legend_name='Crime Rate in San Francisco')
+world_map
+
 
 
 
